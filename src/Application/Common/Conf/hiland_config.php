7a24ad0970cc3813d4b03c8bf6152c86059bdab0
notinmood <develope@live.com>
2017-03-11 08:46:05 +0800

    修改数据库连接

--- src/Application/Common/Conf/hiland_config.php
+++ src/Application/Common/Conf/hiland_config.php
@@ -7,9 +7,9 @@
  */
 
 return array(
-    'PROJECT_NAME' => '老青岛|人文社区服务系统',
-    'PROJECT_NAME_SHORT' => '老青岛|人文社区',
-    'PROJECT_DESCRIPTION' => '让每一个颗心灵都找到归宿。',
+    'PROJECT_NAME' => '解大蛋|健康蛋品专家',
+    'PROJECT_NAME_SHORT' => '解大蛋|蛋品专家',
+    'PROJECT_DESCRIPTION' => '我们只做健康安全的蛋',
 
     'WX_AVATARSERVER_IP'=>'182.254.18.178',//直接使用头像服务器域名wx.qlogo.cn，在阿里云下获取头像容易超时，使用IP改善性能
 );
